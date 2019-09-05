759dad6d0c59a1b51b09f1d3958d004e42cd243e
Ryan Emerson <remerson@redhat.com>
2019-09-05 15:25:41 +0100

    Comment unnecessary methods

--- cachestores/src/main/java/org/infinispan/jmhbenchmarks/JMHBenchmarks.java
+++ cachestores/src/main/java/org/infinispan/jmhbenchmarks/JMHBenchmarks.java
@@ -2,23 +2,17 @@ package org.infinispan.jmhbenchmarks;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Predicate;
 
 import org.infinispan.commons.marshall.Marshaller;
-import org.infinispan.commons.util.IntSet;
 import org.infinispan.container.entries.InternalCacheEntry;
-import org.infinispan.distribution.ch.KeyPartitioner;
 import org.infinispan.marshall.persistence.impl.MarshalledEntryUtil;
-import org.infinispan.persistence.spi.AdvancedLoadWriteStore;
 import org.infinispan.persistence.spi.MarshallableEntry;
-import org.infinispan.persistence.spi.SegmentedAdvancedLoadWriteStore;
 import org.infinispan.test.fwk.TestInternalCacheEntryFactory;
 import org.infinispan.util.concurrent.CompletionStages;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.infra.Blackhole;
 
 import io.reactivex.Flowable;
 
@@ -58,88 +52,88 @@ public class JMHBenchmarks {
 	public Object testLoad(InfinispanHolder holder, KeySequenceGenerator generator) {
 		return holder.getStore().loadEntry(generator.getNextKey());
 	}
-
-	@Benchmark
-	public boolean testContains(InfinispanHolder holder, KeySequenceGenerator generator) {
-		return holder.getStore().contains(generator.getNextKey());
-	}
-
-	@Benchmark
-	public int testSize(InfinispanHolder holder) {
-		return holder.getStore().size();
-	}
-
-	@Benchmark
-	public void testPublishKeysTrue(Blackhole blackhole, InfinispanHolder holder) {
-		Flowable.fromPublisher(holder.getStore().publishKeys(f -> true))
-				.blockingForEach(blackhole::consume);
-	}
-
-	@Benchmark
-	public void testPublishKeysFalse(Blackhole blackhole, InfinispanHolder holder) {
-		Flowable.fromPublisher(holder.getStore().publishKeys(f -> false))
-				.blockingForEach(blackhole::consume);
-	}
-
-	@Benchmark
-	public void testPublishEntriesTrue(Blackhole blackhole, InfinispanHolder holder) {
-		Flowable.fromPublisher(holder.getStore().entryPublisher(f -> true, true, true))
-				.blockingForEach(blackhole::consume);
-	}
-
-	@Benchmark
-	public void testPublishEntriesFalse(Blackhole blackhole, InfinispanHolder holder) {
-		Flowable.fromPublisher(holder.getStore().entryPublisher(f -> false, true, true))
-				.blockingForEach(blackhole::consume);
-	}
-
-	<R> void keysPublisherHalfSegments(Blackhole blackhole, InfinispanHolder holder,
-			Predicate<Object> predicate) {
-		AdvancedLoadWriteStore alws = holder.getStore();
-		IntSet intSet = holder.getHalfSegments();
-		KeyPartitioner keyPartitioner = holder.getKeyPartitioner();
-		if (alws instanceof SegmentedAdvancedLoadWriteStore) {
-			Flowable.fromPublisher(((SegmentedAdvancedLoadWriteStore) alws).publishKeys(intSet, predicate))
-					.blockingForEach(blackhole::consume);
-		} else {
-			Predicate<? super R> segmentFilter = k -> intSet.contains(keyPartitioner.getSegment(k));
-			Flowable.fromPublisher(alws.publishKeys(segmentFilter.and(predicate)))
-					.blockingForEach(blackhole::consume);
-		}
-	}
-
-	void entriesPublisherHalfSegments(Blackhole blackhole, InfinispanHolder holder,
-			Predicate<Object> predicate) {
-		AdvancedLoadWriteStore alws = holder.getStore();
-		IntSet intSet = holder.getHalfSegments();
-		KeyPartitioner keyPartitioner = holder.getKeyPartitioner();
-		if (alws instanceof SegmentedAdvancedLoadWriteStore) {
-			Flowable.fromPublisher(((SegmentedAdvancedLoadWriteStore) alws).entryPublisher(intSet, predicate, true, true))
-					.blockingForEach(blackhole::consume);
-		} else {
-			Predicate<Object> segmentFilter = e -> intSet.contains(keyPartitioner.getSegment(e));
-			Flowable.fromPublisher(alws.entryPublisher(segmentFilter.and(predicate), true, true))
-					.blockingForEach(blackhole::consume);
-		}
-	}
-
-	@Benchmark
-	public void testPublishKeysTrueHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
-		keysPublisherHalfSegments(blackhole, holder, f -> true);
-	}
-
-	@Benchmark
-	public void testPublishKeysFalseHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
-		keysPublisherHalfSegments(blackhole, holder, f -> false);
-	}
-
-	@Benchmark
-	public void testPublishEntriesTrueHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
-		entriesPublisherHalfSegments(blackhole, holder, f -> true);
-	}
-
-	@Benchmark
-	public void testPublishEntriesFalseHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
-		entriesPublisherHalfSegments(blackhole, holder, f -> false);
-	}
+//
+//	@Benchmark
+//	public boolean testContains(InfinispanHolder holder, KeySequenceGenerator generator) {
+//		return holder.getStore().contains(generator.getNextKey());
+//	}
+//
+//	@Benchmark
+//	public int testSize(InfinispanHolder holder) {
+//		return holder.getStore().size();
+//	}
+//
+//	@Benchmark
+//	public void testPublishKeysTrue(Blackhole blackhole, InfinispanHolder holder) {
+//		Flowable.fromPublisher(holder.getStore().publishKeys(f -> true))
+//				.blockingForEach(blackhole::consume);
+//	}
+//
+//	@Benchmark
+//	public void testPublishKeysFalse(Blackhole blackhole, InfinispanHolder holder) {
+//		Flowable.fromPublisher(holder.getStore().publishKeys(f -> false))
+//				.blockingForEach(blackhole::consume);
+//	}
+//
+//	@Benchmark
+//	public void testPublishEntriesTrue(Blackhole blackhole, InfinispanHolder holder) {
+//		Flowable.fromPublisher(holder.getStore().entryPublisher(f -> true, true, true))
+//				.blockingForEach(blackhole::consume);
+//	}
+//
+//	@Benchmark
+//	public void testPublishEntriesFalse(Blackhole blackhole, InfinispanHolder holder) {
+//		Flowable.fromPublisher(holder.getStore().entryPublisher(f -> false, true, true))
+//				.blockingForEach(blackhole::consume);
+//	}
+//
+//	<R> void keysPublisherHalfSegments(Blackhole blackhole, InfinispanHolder holder,
+//			Predicate<Object> predicate) {
+//		AdvancedLoadWriteStore alws = holder.getStore();
+//		IntSet intSet = holder.getHalfSegments();
+//		KeyPartitioner keyPartitioner = holder.getKeyPartitioner();
+//		if (alws instanceof SegmentedAdvancedLoadWriteStore) {
+//			Flowable.fromPublisher(((SegmentedAdvancedLoadWriteStore) alws).publishKeys(intSet, predicate))
+//					.blockingForEach(blackhole::consume);
+//		} else {
+//			Predicate<? super R> segmentFilter = k -> intSet.contains(keyPartitioner.getSegment(k));
+//			Flowable.fromPublisher(alws.publishKeys(segmentFilter.and(predicate)))
+//					.blockingForEach(blackhole::consume);
+//		}
+//	}
+//
+//	void entriesPublisherHalfSegments(Blackhole blackhole, InfinispanHolder holder,
+//			Predicate<Object> predicate) {
+//		AdvancedLoadWriteStore alws = holder.getStore();
+//		IntSet intSet = holder.getHalfSegments();
+//		KeyPartitioner keyPartitioner = holder.getKeyPartitioner();
+//		if (alws instanceof SegmentedAdvancedLoadWriteStore) {
+//			Flowable.fromPublisher(((SegmentedAdvancedLoadWriteStore) alws).entryPublisher(intSet, predicate, true, true))
+//					.blockingForEach(blackhole::consume);
+//		} else {
+//			Predicate<Object> segmentFilter = e -> intSet.contains(keyPartitioner.getSegment(e));
+//			Flowable.fromPublisher(alws.entryPublisher(segmentFilter.and(predicate), true, true))
+//					.blockingForEach(blackhole::consume);
+//		}
+//	}
+//
+//	@Benchmark
+//	public void testPublishKeysTrueHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
+//		keysPublisherHalfSegments(blackhole, holder, f -> true);
+//	}
+//
+//	@Benchmark
+//	public void testPublishKeysFalseHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
+//		keysPublisherHalfSegments(blackhole, holder, f -> false);
+//	}
+//
+//	@Benchmark
+//	public void testPublishEntriesTrueHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
+//		entriesPublisherHalfSegments(blackhole, holder, f -> true);
+//	}
+//
+//	@Benchmark
+//	public void testPublishEntriesFalseHalfSegments(Blackhole blackhole, InfinispanHolder holder) {
+//		entriesPublisherHalfSegments(blackhole, holder, f -> false);
+//	}
 }
